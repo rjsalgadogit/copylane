afdd059371e891518a07fd253336555055afe1d7
Unknown <Unknown@DESKTOP-KO6EVLM>
2025-01-28 18:20:24 +0800

    Added appsettings + notes

--- CopyLane.Server/Program.cs
+++ CopyLane.Server/Program.cs
@@ -1,11 +1,13 @@
 using CopyLane.Infrastructure.Services;
 using CopyLane.Infrastructure.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CopyLane.Server
 {
     internal static class Program
     {
+        public static IConfiguration _configuration { get; set; }
         public static IServiceProvider _serviceProvider { get; private set; }
 
         /// <summary>
@@ -18,6 +20,13 @@ namespace CopyLane.Server
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
+            // Load Configuration
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            _configuration = builder.Build();
+
+            // Dependency Injection
             var serviceCollection = new ServiceCollection();
             ConfigureServices(serviceCollection);
             _serviceProvider = serviceCollection.BuildServiceProvider();
